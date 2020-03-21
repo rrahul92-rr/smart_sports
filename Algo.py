979feed4a43e5e7685b35b0e793447328936adc7
Rahul Ramachandran <rrahul92.rr@gmail.com>
2020-03-21 21:49:21 +0530

    Strike rate calculation

--- Algo.py
+++ Algo.py
@@ -1,2 +1,34 @@
 import pandas as pd
 import numpy as np
+import os
+from collections import OrderedDict
+
+os.chdir("/Users/basingse/Desktop/Sports fantasy/Data")
+print("current working directory :", os.getcwd())
+
+df = pd.read_csv('../Data/T20.csv')
+
+result = list(OrderedDict.fromkeys(list(df.match_key)))
+
+df.to_csv("test3.csv")
+
+df1 = df[(df.match_key == 211028) & (df.innings == "1st innings")]
+
+df =  (df.assign(SR_match=round(100*df.groupby(['match_key','innings'])
+                                 .total_runs
+                                 .expanding()    
+                                 .mean()
+                                 .reset_index(drop=True),2)))
+
+df =  (df.assign(SR_player=round(100*df.groupby(['match_key','innings','batsman'])
+                                 .b_runs
+                                 .expanding()    
+                                 .mean()
+                                 .reset_index(drop=True),2)))
+
+
+
+
+df.head()
+
+
