e700fabdcabd341d40ee5d4b2bd45cb57dc70c22
Rahul Ramachandran <rrahul92.rr@gmail.com>
2020-03-22 22:45:32 +0530

    Player strike rate added

--- Algo.py
+++ Algo.py
@@ -10,20 +10,47 @@ df = pd.read_csv('../Data/T20.csv')
 
 result = list(OrderedDict.fromkeys(list(df.match_key)))
 
-df1 = df[(df.match_key == 211028) & (df.innings == "1st innings")]
+df1 = df[(df.team == "England") &  (df.batsman == "ME Trescothick")]
 
-df =  (df.assign(SR_match=round(100*df.groupby(['match_key','innings'])
+
+### Expanding Match Strike rate ###
+
+df =  (df.assign(SR_match=round(100*df.sort_values(by=["ball"])
+                                 .groupby(['match_key','innings'])
                                  .total_runs
-                                 .expanding()    
+                                 .expanding()
                                  .mean()
                                  .reset_index(drop=True),2)))
 
-df =  (df.assign(SR_player=round(100*df.groupby(['match_key','innings','batsman'])
+### Expanding Player Strike rate ###
+
+
+
+### Momentum Bonus ### 
+
+k = 0.25
+df =  (df.assign(momentum_bonus=round(k*100*df.sort_values(by=["ball"])
+                                 .groupby(['match_key', 'innings'])
+                                 .total_runs
+                                 .rolling(3)
+                                 .mean()
+                                 .reset_index(drop=True),2)))
+
+###########################
+
+df1 =  (df1.assign(SR_player=round(100*df1.sort_values(by=["ball"], ascending=False)
+                                 .groupby(['batsman', 'match_key'])
                                  .b_runs
-                                 .expanding()    
+                                 .expanding()
                                  .mean()
                                  .reset_index(drop=True),2)))
 
-df.head()
+df1.head(10)
+df3.to_csv("test5.csv")
+df1.groupby(['match_key','batsman']).b_runs.expanding().median()
 
-df.to_csv("test3.csv")
+df2 = df1.groupby(['match_key','batsman'])['b_runs'].expanding().mean().reset_index(drop=False)
+df2.rename(columns={"b_runs" : "SR_Player"})
+df3 = df1.merge(df2, left_on=['match_key','batsman','Unnamed: 0'], right_on=['match_key','batsman','level_2'])
+
+
